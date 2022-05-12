c24ab158e289040a4ac6d140707f5fa48914d117
THEnter <ez_25_25@yahoo.com>
2022-05-12 14:21:20 -0500

    13.6 commit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,17 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,5 +33,19 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    //lives at /events/create(its okay for this and last method to have the same path because one
+    //uses GET and this is POST
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        //eventName is name of the text input given in create.html
+
+        events.add(eventName);
+
+        //return a redirect reponse that instructs the browser to go to a different page
+        //redirect: means redirect to root path for this controller
+        return  "redirect:";
+
     }
 }
