c60c8935b0ccd0cf8a3e64c7b766d2256c00b2b5
THEnter <ez_25_25@yahoo.com>
2022-05-16 12:17:58 -0500

    new branch, events now HashMap Ch 13 exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    //events is now a HashMap ch 13 exercise
+    private static HashMap<String,String> events = new HashMap<>();
+
 
     @GetMapping
     public String displayAllEvents(Model model){
@@ -38,10 +42,10 @@ public class EventController {
     //lives at /events/create(its okay for this and last method to have the same path because one
     //uses GET and this is POST
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDesc) {
         //eventName is name of the text input given in create.html
 
-        events.add(eventName);
+        events.put(eventName,eventDesc);
 
         //return a redirect reponse that instructs the browser to go to a different page
         //redirect: means redirect to root path for this controller
