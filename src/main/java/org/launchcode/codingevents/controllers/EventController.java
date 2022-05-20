3a5c797decb7c364acdbc575becb7c0bba1950e9
THEnter <ez_25_25@yahoo.com>
2022-05-20 12:59:02 -0500

    refactoring w/ model binding

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,10 +38,12 @@ public class EventController {
     //lives at /events/create(its okay for this and last method to have the same path because one
     //uses GET and this is POST
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+    public String createEvent(@ModelAttribute Event newEvent) {
+        //ModelAttribute is the key piece of model binding, have to back and change form names so Spring can find name and description and binding can work
+
         //eventName is name of the text input given in create.html
 
-        EventData.addNew(new Event(eventName, eventDescription));
+        EventData.addNew(newEvent);
 
         //return a redirect reponse that instructs the browser to go to a different page
         //redirect: means redirect to root path for this controller
