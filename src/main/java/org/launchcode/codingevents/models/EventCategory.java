03434e68254c3eeafa563c1f7f56ac189cabb5a1
THEnter <ez_25_25@yahoo.com>
2022-06-17 16:44:35 -0500

    finished ORM relationship categories to events(one-to-many)

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,8 +3,11 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -13,6 +16,10 @@ public class EventCategory extends AbstractEntity {
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     @NotBlank(message = "Name is required")
     private String name;
+
+    //tells hibernate which events are in any given category
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(String name) {
         this.name = name;
@@ -28,6 +35,9 @@ public class EventCategory extends AbstractEntity {
         this.name = name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 
     @Override
     public String toString() {
